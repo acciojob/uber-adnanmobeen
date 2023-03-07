76a15f0961fd24e89c1e2dc5f864ec7cdb6414fd
adnan <adnanmobeen444@gmail.com>
2023-03-07 21:10:19 +0530

    complete

---
+++ src/main/java/com/driver/model/TripBooking.java
@@ -0,0 +1,108 @@
+package com.driver.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tripbookings")
+public class TripBooking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int tripBookingId;
+
+    private String fromLocation;
+
+    private String toLocation;
+
+    private int distanceInKm;
+
+    private TripStatus Status;
+
+    private int bill;
+
+    @ManyToOne
+    @JoinColumn
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn
+    private Driver driver;
+
+    public TripBooking() {
+    }
+
+    public TripBooking(int tripBookingId, String fromLocation, String toLocation, int distanceInKm, TripStatus status, int bill, Customer customer, Driver driver) {
+        this.tripBookingId = tripBookingId;
+        this.fromLocation = fromLocation;
+        this.toLocation = toLocation;
+        this.distanceInKm = distanceInKm;
+        Status = status;
+        this.bill = bill;
+        this.customer = customer;
+        this.driver = driver;
+    }
+
+    public int getTripBookingId() {
+        return tripBookingId;
+    }
+
+    public void setTripBookingId(int tripBookingId) {
+        this.tripBookingId = tripBookingId;
+    }
+
+    public String getFromLocation() {
+        return fromLocation;
+    }
+
+    public void setFromLocation(String fromLocation) {
+        this.fromLocation = fromLocation;
+    }
+
+    public String getToLocation() {
+        return toLocation;
+    }
+
+    public void setToLocation(String toLocation) {
+        this.toLocation = toLocation;
+    }
+
+    public int getDistanceInKm() {
+        return distanceInKm;
+    }
+
+    public void setDistanceInKm(int distanceInKm) {
+        this.distanceInKm = distanceInKm;
+    }
+
+    public TripStatus getStatus() {
+        return Status;
+    }
+
+    public void setStatus(TripStatus status) {
+        Status = status;
+    }
+
+    public int getBill() {
+        return bill;
+    }
+
+    public void setBill(int bill) {
+        this.bill = bill;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+}
